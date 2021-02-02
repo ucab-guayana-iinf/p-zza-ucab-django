baca4a9389aaef91320708785193e86cb679699d
Juan Marcos <juanmarcos.vera12@gmail.com>
2021-02-02 13:59:38 -0400

    Agregar templates y views genericas y mapeo de urls

--- src/pizzeria_backend/urls.py
+++ src/pizzeria_backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('pizzeria/', include('pizzeria_frontend.urls')),
+    # path('pizzeria/', include('pizzeria_frontend.urls')),
+    path('', include('pizzeria_frontend.urls')),
     path('admin/', admin.site.urls),
 ]
--- src/pizzeria_frontend/views.py
+++ src/pizzeria_frontend/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
+from .models import Order, Pizza
 
-def index(request):
-    return HttpResponse("Hola")
+# def index(request):
+#     return HttpResponse("Hola")
+
+class IndexView(generic.ListView):
+    template_name = 'pizzeria_frontend/index.html'
+    context_object_name = 'orders_list'
+
+    def get_queryset(self):
+        """Return orders."""
+        return Order.objects.all
+
+class DetailView(generic.DetailView):
+    model = Order
+    template_name = 'pizzeria_frontend/detail.html'
