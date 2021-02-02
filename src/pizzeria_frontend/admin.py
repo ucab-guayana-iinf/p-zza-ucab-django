43b3ce37dfd61a9f9582c78ef38925cfb23bcfd2
Juan Marcos <juanmarcos.vera12@gmail.com>
2021-02-02 13:59:07 -0400

    Crear modelos y registrarlos en el admin

--- src/pizzeria_frontend/admin.py
+++ src/pizzeria_frontend/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Topping, Order, Pizza, Size
 
 # Register your models here.
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'size', 'price', 'order']
+
+class PizzaInline(admin.TabularInline):
+    model = Pizza
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    # readonly_fields = ['date']
+    fieldsets = [
+        (None, {'fields': ['client', 'price']}),
+        # ('Fecha de publicación', {'fields': ['pub_date'], 'classes': ['collapseXDDDDDDDD']}),
+    ]
+    inlines = [PizzaInline]
+    list_display = ['id', 'client', 'price', 'date']
+    # list_filter = ['pub_date']
+    # search_fields = ['question_text']
+
+admin.site.register(Topping)
+admin.site.register(Size)
+admin.site.register(Pizza, PizzaAdmin)
+admin.site.register(Order, OrderAdmin)
--- src/pizzeria_frontend/models.py
+++ src/pizzeria_frontend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f'{self.name} ({self.price})'
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f'{self.name} ({self.price})'
+
+class Order(models.Model):
+    client = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Orden de {self.client} por {self.price} el {self.date}'
+
+class Pizza(models.Model):
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    toppings = models.ManyToManyField(Topping)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Pizza {self.size}'
+
